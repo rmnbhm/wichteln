b9121f52306f0f3e35c97831cbc3467927748a41
Roman Böhm <romanboehm@posteo.de>
2020-10-04 20:10:59 +0200

    Protect match invariant more obviously
    * The "retry when encountering an exception upon instantiating a match
    between a participant and themselves" mechanism worked sufficiently,
    but it was not very obvious about the invariant it protected: namely
    a `ParticipantsMatch` requires the donor and the recipient to be
    different `Participant` instances. Therefore -- even though it is
    actually superfluous -- I added an extra guard in the calling class to
    shine light on what is being done and prevent future changes from
    breaking the matching algorithm (e.g. when the class itself might not
    protect invariants anymore).

--- src/main/java/com/rmnbhm/wichteln/service/ParticipantsMatcher.java
+++ src/main/java/com/rmnbhm/wichteln/service/ParticipantsMatcher.java
@@ -7,7 +7,10 @@ import com.rmnbhm.wichteln.model.ParticipantsMatch.Recipient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,17 +21,19 @@ public class ParticipantsMatcher {
     public List<ParticipantsMatch> match(List<Participant> participants) {
         List<Participant> copy = new ArrayList<>(participants);
         Random random = new Random();
-        while (true) {
+        do {
             Collections.rotate(copy, random.nextInt());
-            try {
-                return IntStream.range(0, participants.size())
-                        .mapToObj(i ->
-                                new ParticipantsMatch(new Donor(participants.get(i)), new Recipient(copy.get(i)))
-                        ).collect(Collectors.toList());
-            } catch (IllegalArgumentException ignored) {
-                log.debug("Collision while shuffling participants to generate matches");
-            }
-        }
+        } while (areNotMatchedCorrectly(participants, copy));
+
+        return IntStream.range(0, participants.size())
+                .mapToObj(i ->
+                        new ParticipantsMatch(new Donor(participants.get(i)), new Recipient(copy.get(i)))
+                ).collect(Collectors.toList());
+    }
+
+    private boolean areNotMatchedCorrectly(List<Participant> participants, List<Participant> copy) {
+        return IntStream.range(0, participants.size())
+                .anyMatch(i -> participants.get(i).equals(copy.get(i)));
     }
 
 }
