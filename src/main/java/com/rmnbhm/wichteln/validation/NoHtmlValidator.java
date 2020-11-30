8773dc56357c7b1be34f65b1f2da1a60db13bdde
Roman Böhm <romanboehm@posteo.de>
2020-11-30 11:29:38 +0100

    Make custom validators null-safe

--- src/main/java/com/rmnbhm/wichteln/validation/NoHtmlValidator.java
+++ src/main/java/com/rmnbhm/wichteln/validation/NoHtmlValidator.java
@@ -7,12 +7,18 @@ import org.slf4j.LoggerFactory;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import static java.util.Objects.isNull;
+
 public class NoHtmlValidator implements ConstraintValidator<NoHtml, String> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NoHtmlValidator.class);
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        // This validator is not concerned with null checks.
+        if (isNull(value)) {
+            return true;
+        }
         String htmlEscaped = StringEscapeUtils.escapeHtml4(value);
         // Based on the simple assumption that the escaped string needs to be longer due to the nature of escaping,
         // e.g. "<" turns to "&lt;".
--- src/main/java/com/rmnbhm/wichteln/validation/NoJavaControlCharactersValidator.java
+++ src/main/java/com/rmnbhm/wichteln/validation/NoJavaControlCharactersValidator.java
@@ -7,12 +7,18 @@ import org.slf4j.LoggerFactory;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import static java.util.Objects.isNull;
+
 public class NoJavaControlCharactersValidator implements ConstraintValidator<NoJavaControlCharacters, String> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NoJavaControlCharactersValidator.class);
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        // This validator is not concerned with null checks.
+        if (isNull(value)) {
+            return true;
+        }
         String javaEscaped = StringEscapeUtils.escapeJava(value);
         // Based on the simple assumption that the escaped string needs to be longer due to the nature of escaping,
         // e.g. "\n" turns to "\\n".
