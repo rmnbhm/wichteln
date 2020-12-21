f754d72f13e1425d04778f45f4355d1f3a6af9a6
Roman Böhm <romanboehm@posteo.de>
2020-12-21 19:49:20 +0100

    Remove unused logger

--- src/main/java/com/rmnbhm/wichteln/service/WichtelnService.java
+++ src/main/java/com/rmnbhm/wichteln/service/WichtelnService.java
@@ -3,8 +3,6 @@ package com.rmnbhm.wichteln.service;
 import com.rmnbhm.wichteln.model.Event;
 import com.rmnbhm.wichteln.model.ParticipantsMatch;
 import com.rmnbhm.wichteln.model.SendResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,7 +11,6 @@ import java.util.List;
 @Service
 public class WichtelnService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(WichtelnService.class);
     private final ParticipantsMatcher matcher;
     private final WichtelnMailer mailSender;
 
